f09af38848f515a28029c4108b61528172caad55
Mark Hayes <mark@deployfx.com>
2012-02-02 18:44:48 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awesomeapp::Application.config.secret_token = '8bab83e5cb07c1362405437849d0bdbd2cfedc4c70347e89f7d123816ae5df0d47c99ad954bde7eadd6e9a2a5c00967cd53c816d31169641065740f635a038dc'
